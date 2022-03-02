158ad89a406898a90963a6fc4f8a3fbbcd3a6b6c
Aaron <ken888686@gmail.com>
2022-03-02 23:51:39 +0800

    vue init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="nav">
+    <router-link to="/">
+      Home
+    </router-link> |
+    <router-link to="/about">
+      About
+    </router-link>
+  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
@@ -15,7 +19,7 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 
   a {
